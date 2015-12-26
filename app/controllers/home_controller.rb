41d333acf9dfbe6a1a1aeb5b436ca6c3617e016b
kazunoriyokoyama <info@ultrac.co.jp>
2015-12-26 06:44:35 +0000

    1226

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    # render 'login' unless user_signed_in?
+    # redirect_to new_user_session_url unless user_signed_in?
+    
   end
+
 end
