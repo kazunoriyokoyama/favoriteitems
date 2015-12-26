2872398fa3d23e894805d794db511cecb09ed6b1
kazunoriyokoyama <info@ultrac.co.jp>
2015-12-26 11:51:58 +0000

    12268

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151222131901) do
+ActiveRecord::Schema.define(version: 20151226114144) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string   "name"
+    t.string   "body"
+    t.integer  "sex"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
