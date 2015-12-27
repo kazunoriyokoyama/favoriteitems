59ae9fd3ec764719d7973bb32825a13f542e9478
kazunoriyokoyama <info@ultrac.co.jp>
2015-12-27 01:08:32 +0000

    1227

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,12 @@ class HomeController < ApplicationController
     # render 'login' unless user_signed_in?
     # redirect_to new_user_session_url unless user_signed_in?
     
+    if user_signed_in?
+    redirect_to messages_url
+    else
+    render 'login'
+    end
+  
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
 
+
   devise_for :users, path_names: { sign_in: "login", sign_out: "logout"},
   controllers: { omniauth_callbacks: "omniauth_callbacks" }
   
-  get 'home/index'
 
 root to: "home#index"
+resources :messages
 end
